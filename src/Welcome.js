bdb6e245055e6c4e0d40ed097d0ae79051460474
Camden Bickel <cam@bickel.io>
2018-12-12 15:55:36 -0500

    styling for landing page

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,38 +2,45 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import styles from './Welcome.scss';
+
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      code: '',
+      code: 'DEFAULT (Change me)',
     };
   }
 
   handleCodeChange = event => {
     this.setState({
-      code: event.target.value,
+      code: event.target.value.toLowerCase(),
     });
   };
 
   render() {
     const linkTo = `/log?code=${this.state.code}`;
     return (
-      <div>
-        <h1>Welcome to the site</h1>
-        <label htmlFor="code">Your piff code: </label>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Say hi to Piff.</h1>
+        <label className={styles.inputLabel} htmlFor="code">
+          Enter your Piff's unique code:
+        </label>
         <input
+          className={styles.codeInput}
           type="text"
           name="code"
           value={this.state.code}
           onChange={this.handleCodeChange}
         />
-        {this.state.code ? (
-          <Link to={linkTo}>Continue</Link>
-        ) : (
-          <p>Enter a code to continue!</p>
-        )}
+        <div>
+          <Link to={linkTo}>
+            <button className={styles.ctaButton} disabled={!this.state.code}>
+              Continue
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
