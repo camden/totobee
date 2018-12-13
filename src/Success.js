baf386baa0b832e07803b9f9b85c6a292a445b5d
Camden Bickel <cam@bickel.io>
2018-12-13 01:21:53 -0500

    delete user on success

--- src/Success.js
+++ src/Success.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Firebase } from './firebase';
 
 import styles from './Success.scss';
 
-const Success = () => {
-  return (
-    <div className={styles.container}>
-      <h1>Success!</h1>
-      <p>Your Totobee has been logged successfully.</p>
-      <p>
-        Now, put your Totobee somewhere so another lucky person can find it!
-      </p>
-    </div>
-  );
-};
+class Success extends React.Component {
+  componentDidMount() {
+    const user = firebase.auth().currentUser;
+    user.delete();
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1>Success!</h1>
+        <p>Your Totobee has been logged successfully!</p>
+        <p>
+          Now, put your Totobee somewhere so another lucky person can find it!
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Success;
