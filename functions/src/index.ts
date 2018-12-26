3e2292d3bf34efb8d456c5819263fe4d517b342b
Camden Bickel <cam@bickel.io>
2018-12-25 18:37:54 -0800

    add function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as NearbyCities from 'cities';
+import fetch from 'node-fetch';
 
 exports.findCities = functions.https.onCall((data, context) => {
   const positions = data.positions;
@@ -9,3 +10,13 @@ exports.findCities = functions.https.onCall((data, context) => {
     cities,
   };
 });
+
+exports.notifyVisit = functions.firestore
+  .document('visits/{visit}')
+  .onCreate((snap, context) => {
+    const visit = snap.data();
+    const webhookUrl =
+      'https://maker.ifttt.com/trigger/visit_created/with/key/ozV7HJWweMSFh4aT9Irzm1TqzTHgJfmOosEY_TrWQzP';
+
+    // fetch(webhookUrl);
+  });
